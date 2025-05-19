00e364c6cc856eb426e829b32f0bf078565bcdbb
Chad Miller <chadtmiller15@gmail.com>
2025-05-18 22:55:23 -0700

    fix again

--- packages/bff/components/Layout.tsx
+++ packages/bff/components/Layout.tsx
@@ -74,7 +74,7 @@ const LayoutNav: FunctionalComponent<LayoutNavProps> = (
               </form>
               <Button asChild variant="secondary" class="tw:relative tw:pl-2">
                 <a href="/notifications">
-                  <i class="fas fa-bell tw:text-zinc-950 tw:dark-zinc-50" />
+                  <i class="fas fa-bell tw:text-zinc-950 tw:dark:text-zinc-50" />
                   {hasNotifications
                     ? (
                       <span class="tw:absolute tw:inline-flex tw:items-center tw:justify-center tw:w-3 tw:h-3 tw:text-xs tw:font-bold tw:text-white tw:bg-sky-500 tw:rounded-full tw:top-1 tw:right-1" />
